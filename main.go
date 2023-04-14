94f774d88989e3fe573efb7a51c472da36109044
tbz <tianbaozhen@foxmail.com>
2023-04-14 10:19:56 +0800

    更新

--- main.go
+++ main.go
@@ -1,30 +1,24 @@
 package engine // import "m7s.live/engine/v4"
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net"
-	"net/http"
 	"os"
 	"path/filepath"
 	"reflect"
-	"runtime"
 	"strings"
 	"time"
 
-	"github.com/denisbrodbeck/machineid"
-	"github.com/google/uuid"
 	. "github.com/logrusorgru/aurora"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 	"gopkg.in/yaml.v3"
 	"m7s.live/engine/v4/config"
+	"m7s.live/engine/v4/lang"
 	"m7s.live/engine/v4/log"
 	"m7s.live/engine/v4/util"
-	"m7s.live/engine/v4/lang"
 )
 
 var (
@@ -55,7 +49,7 @@ func init() {
 
 // Run 启动Monibuca引擎，传入总的Context，可用于关闭所有
 func Run(ctx context.Context, configFile string) (err error) {
-	id, _ := machineid.ProtectedID("monibuca")
+	//id, _ := machineid.ProtectedID("monibuca")
 	SysInfo.StartTime = time.Now()
 	SysInfo.Version = Engine.Version
 	Engine.Context = ctx
@@ -120,11 +114,11 @@ func Run(ctx context.Context, configFile string) (err error) {
 		}
 		plugin.assign()
 	}
-	UUID := uuid.NewString()
-	reportTimer := time.NewTicker(time.Minute)
-	contentBuf := bytes.NewBuffer(nil)
-	req, _ := http.NewRequestWithContext(ctx, http.MethodPost, "https://console.monibuca.com/report", nil)
-	req.Header.Set("Content-Type", "application/json")
+	//UUID := uuid.NewString()
+	//reportTimer := time.NewTicker(time.Minute)
+	//contentBuf := bytes.NewBuffer(nil)
+	//req, _ := http.NewRequestWithContext(ctx, http.MethodPost, "https://console.monibuca.com/report", nil)
+	//req.Header.Set("Content-Type", "application/json")
 	version := Engine.Version
 	if ver, ok := ctx.Value("version").(string); ok && ver != "" && ver != "dev" {
 		version = ver
@@ -167,21 +161,21 @@ func Run(ctx context.Context, configFile string) (err error) {
 	fmt.Println(Bold(Cyan("开发文档: ")), Yellow("https://m7s.live/devel/startup.html"))
 	fmt.Println(Bold(Cyan("视频教程: ")), Yellow("https://space.bilibili.com/328443019/channel/collectiondetail?sid=514619"))
 	fmt.Println(Bold(Cyan("远程界面: ")), Yellow("https://console.monibuca.com"))
-	rp := struct {
-		UUID     string `json:"uuid"`
-		Machine  string `json:"machine"`
-		Instance string `json:"instance"`
-		Version  string `json:"version"`
-		OS       string `json:"os"`
-		Arch     string `json:"arch"`
-	}{UUID, id, EngineConfig.GetInstanceId(), version, runtime.GOOS, runtime.GOARCH}
-	json.NewEncoder(contentBuf).Encode(&rp)
-	req.Body = ioutil.NopCloser(contentBuf)
+	//rp := struct {
+	//	UUID     string `json:"uuid"`
+	//	Machine  string `json:"machine"`
+	//	Instance string `json:"instance"`
+	//	Version  string `json:"version"`
+	//	OS       string `json:"os"`
+	//	Arch     string `json:"arch"`
+	//}{UUID, id, EngineConfig.GetInstanceId(), version, runtime.GOOS, runtime.GOARCH}
+	//json.NewEncoder(contentBuf).Encode(&rp)
+	//req.Body = ioutil.NopCloser(contentBuf)
 	if EngineConfig.Secret != "" {
 		EngineConfig.OnEvent(ctx)
 	}
-	var c http.Client
-	c.Do(req)
+	//var c http.Client
+	//c.Do(req)
 	for {
 		select {
 		case event := <-EventBus:
@@ -193,11 +187,11 @@ func Run(ctx context.Context, configFile string) (err error) {
 			EngineConfig.OnEvent(event)
 		case <-ctx.Done():
 			return
-		case <-reportTimer.C:
-			contentBuf.Reset()
-			contentBuf.WriteString(fmt.Sprintf(`{"uuid":"`+UUID+`","streams":%d}`, len(Streams.Map)))
-			req.Body = ioutil.NopCloser(contentBuf)
-			c.Do(req)
+			//case <-reportTimer.C:
+			//	contentBuf.Reset()
+			//	contentBuf.WriteString(fmt.Sprintf(`{"uuid":"`+UUID+`","streams":%d}`, len(Streams.Map)))
+			//	req.Body = ioutil.NopCloser(contentBuf)
+			//	c.Do(req)
 		}
 	}
 }
