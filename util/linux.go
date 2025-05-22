107037765628d13f3429220ee31f90ed87da960f
tbz <tianbaozhen@foxmail.com>
2025-05-22 17:27:46 +0800

    remove fatal log and shutdown script

--- util/linux.go
+++ util/linux.go
@@ -1,13 +1,9 @@
+//go:build !windows
 // +build !windows
 
 package util
 
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-)
-
-func CreateShutdownScript() error{
-	return ioutil.WriteFile("shutdown.sh", []byte(fmt.Sprintf("kill -9 %d", os.Getpid())), 0777)
+func CreateShutdownScript() error {
+	//return ioutil.WriteFile("shutdown.sh", []byte(fmt.Sprintf("kill -9 %d", os.Getpid())), 0777)
+	return nil
 }
